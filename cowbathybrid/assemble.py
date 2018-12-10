689208150768f3f7f07629383baaf1d9feceb019
Andrew Low <andrew.low@canada.ca>
2018-12-10 16:30:57 -0500

    Add discard_unassigned to porechop demux, on the off chance that unassigned reads end up having more than any barcode

--- cowbathybrid/assemble.py
+++ cowbathybrid/assemble.py
@@ -85,9 +85,9 @@ def run_porechop(minion_reads, output_directory, threads, logfile=None):
     # Will just implement a size check on each of the output files we get and assume the biggest one is right.
     chopped_reads = os.path.join(output_directory, 'minION_chopped.fastq.gz')
     demultiplex_dir = os.path.join(output_directory, 'demultiplex')
-    cmd = 'porechop -i {minion_reads} -b {demux_dir} -t {threads}'.format(minion_reads=minion_reads,
-                                                                          demux_dir=demultiplex_dir,
-                                                                          threads=threads)
+    cmd = 'porechop -i {minion_reads} -b {demux_dir} -t {threads} --discard_unassigned'.format(minion_reads=minion_reads,
+                                                                                               demux_dir=demultiplex_dir,
+                                                                                               threads=threads)
     run_cmd(cmd, logfile=logfile)
 
     # Now that we've demultiplexed and chopped the FASTQs, figure out which barcode is the one we want.
