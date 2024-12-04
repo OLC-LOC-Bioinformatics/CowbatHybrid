e73b705fd42c8a0eec764e249b2a1adb472d86e0
Madhubioinfo <mmadhubioinfo@gmail.com>
2024-12-04 16:27:46 -0500

    Update quality.py
    previous command line was compatible with old version nanopore and updated here

--- cowbathybrid/quality.py
+++ cowbathybrid/quality.py
@@ -7,7 +7,7 @@ import os
 def run_nanoplot(fastq_file, output_directory, threads):
     if os.path.isfile(os.path.join(output_directory, 'NanoPlot-report.html')):
         return
-    cmd = 'NanoPlot -t {threads} -o {output_directory} --fastq_rich {fastq_file}'.format(threads=threads,
+    cmd = 'NanoPlot -t {threads} -o {output_directory} --fastq {fastq_file}'.format(threads=threads,
                                                                                          output_directory=output_directory,
                                                                                          fastq_file=fastq_file)
     run_cmd(cmd)
