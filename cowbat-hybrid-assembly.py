f9b13ce47d03952b6762fbe30502931a9da40458
Madhubioinfo <mmadhubioinfo@gmail.com>
2026-03-02 11:28:39 -0500

    added updated hybrid assembly code

--- cowbat-hybrid-assembly.py
+++ cowbat-hybrid-assembly.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/mnt/nas2/virtual_environments/cowbat_hybrid/bin/python3.9
 
 """
-Wrapper script for the cowbat-hybrid pipeline. This script will take a CSV file with the following headers:
-MinION, Illumina_R1, Illumina_R2, and OutName. For MinION, Illumina_R1, and Illumina_R2, the full path to the read
-file for each sample should be present. OutName is what you want your assembly to be called. This script will run
-nanoplot on the MinION reads, run flye on the MinION reads, and then run hybrid assemblies with Unicycler. Finally,
-it will run assembly typing on the assemblies with the cowbat pipeline.
+Wrapper script for the cowbat-hybrid pipeline.   This script will take a CSV file with the following headers:
+MinION, Illumina_R1, Illumina_R2, and OutName.   For MinION, Illumina_R1, and Illumina_R2, the full path to the read
+file for each sample should be present.  OutName is what you want your assembly to be called.  This script will run
+nanoplot on the MinION reads, run flye on the MinION reads, and then run hybrid assemblies with Unicycler.   Finally,
+it will run assembly typing on the assemblies with the cowbat pipeline.  
 """
 
 # Standard imports
@@ -13,6 +13,7 @@ import argparse
 import logging
 import multiprocessing
 import os
+import shutil
 import time
 
 # Third-party imports
@@ -72,8 +73,30 @@ if __name__ == '__main__':
         version=__version__
     )
     parser.add_argument(
+        '--asm-coverage',
+        required=False,
+        type=int,
+        default=None,               # FIXED: None instead of False
+        dest='asm_coverage',        # FIXED: explicit dest so args.asm_coverage works
+        help='Reduced coverage for initial disjointig assembly (e.g. 50). '
+             'Optional - set if flye is not producing any disjointigs. '
+             'Recommended to use together with --genome-size.'
+    )
+    parser.add_argument(
+        '-g', '--genome-size',
+        required=False,
+        type=str,                   # FIXED: str instead of int (values like 5m, 1.7m, 500k)
+        default=None,               # FIXED: None instead of False
+        dest='genome_size',         # FIXED: explicit dest so args.genome_size works
+        help='For flye - Estimated genome size of assembled organism '
+             '(e.g. 5m, 3m, 1.7m, 500k). '
+             'Optional - if not provided, Flye estimates automatically. '
+             'Required if using --asm-coverage flag.'
+    )
+    parser.add_argument(
         '-f', '--filter_reads',
         type=int,
+        default=None,
         help='Unicycler can be pretty darn slow if given very large read sets. With this option, you can specify the '
              'number of long read bases you want to use. Aiming for 40X-50X depth for your target organism seems to '
              'work pretty well.'
@@ -82,13 +105,20 @@ if __name__ == '__main__':
         '-c', '--conservative',
         default=False,
         action="store_true",
-        help="Run Unicycler in conservative mode. Get more contigs, but fewer mis-assemblies"
+        help="Run Unicycler in conservative mode. Get more contigs, but fewer mis-assemblies."
     )
     args = parser.parse_args()
     SetupLogging(debug=args.verbose)
 
     if check_dependencies() is False:
         quit(code=1)
+
+    # Log genome size and asm coverage if provided
+    if args.genome_size:
+        logging.info('Genome size set to: %s', args.genome_size)
+    if args.asm_coverage:
+        logging.info('Flye assembly coverage set to: %s', args.asm_coverage)
+
     # Parse the input CSV file we were given. This returns a list of SequenceFileInfo objects,
     # which have illumina_r1, illumina_r2, minion_reads, and outname as attributes.
     sequence_file_info_list = parse_hybrid_csv(args.input_csv)
@@ -106,10 +136,9 @@ if __name__ == '__main__':
             output_directory=nanoplot_out_dir
         )
 
-    # Initialize the output directory for the flye assemblies
-    flye_out_dir = None
-
-    ###Madhu: run fly assemblies to generate long contigs####
+    # Run Flye assemblies to generate long contigs
+    # Store flye output paths for each sample
+    flye_outputs = {}
     for sequence_file_info in sequence_file_info_list:
         flye_out_dir = os.path.join(args.output_directory, sequence_file_info.outname, 'flye')
         if not os.path.isdir(flye_out_dir):
@@ -118,25 +147,29 @@ if __name__ == '__main__':
         run_flye(
             fastq_file=sequence_file_info.minion_reads,
             threads=args.threads,
-            output_directory=flye_out_dir
+            output_directory=flye_out_dir,
+            genome_size=args.genome_size,       # FIXED: uncommented and corrected
+            asm_coverage=args.asm_coverage      # FIXED: uncommented and corrected
         )
+        # Store the flye output path for this specific sample
+        flye_outputs[sequence_file_info.outname] = os.path.join(flye_out_dir, 'assembly.fasta')
 
-    # Now run assemblies - intermediate files will be in output_directory/samplename/assembly, completed fasta file
-    # will be in output_directory/samplename.fasta
-    best_assemblies_dir = os.path.join(args.output_directory)
-    flye_output = os.path.join(flye_out_dir, 'assembly.fasta')
-    gfa_files_dir = os.path.join(args.output_directory) #madhu added this
+    # Now run assemblies - intermediate files will be in output_directory/samplename/assembly
+    # completed fasta file will be in output_directory/samplename.fasta
+    best_assemblies_dir = os.path.join(args.output_directory, 'BestAssemblies')
+    gfa_files_dir = os.path.join(args.output_directory, 'GFA_files')
 
-    # Create the output directory if it doesn't exist
+    # Create the output directories if they don't exist
     os.makedirs(best_assemblies_dir, exist_ok=True)
+    os.makedirs(gfa_files_dir, exist_ok=True)
 
     logging.info('Running Unicycler on samples...')
 
-    # Run Unicycler on each of the samples
+    # Run Unicycler on each of the samples with the correct flye output for that sample
     for sequence_file_info in sequence_file_info_list:
         assemble.run_hybrid_assembly(
             long_reads=sequence_file_info.minion_reads,
-            flye_contigs=flye_output,
+            flye_contigs=flye_outputs[sequence_file_info.outname],
             forward_short_reads=sequence_file_info.illumina_r1,
             reverse_short_reads=sequence_file_info.illumina_r2,
             output_directory=os.path.join(args.output_directory, sequence_file_info.outname, 'assembly'),
@@ -158,16 +191,29 @@ if __name__ == '__main__':
     )
     typer.main()
 
+    # Clean up nested BestAssemblies directory created by COWBAT
+    nested_best_assemblies = os.path.join(best_assemblies_dir, 'BestAssemblies')
+    if os.path.isdir(nested_best_assemblies):
+        logging.info('Cleaning up nested BestAssemblies directory...')
+        shutil.rmtree(nested_best_assemblies)
+        logging.info('Nested BestAssemblies removed.')
+
     logging.info('Creating reports...')
     samples = [
-        Sample(name=sequence_file_info.outname, datastore='datastore', out_dir=os.path.join(args.output_directory, sequence_file_info.outname))
+        Sample(
+            name=sequence_file_info.outname,
+            datastore='datastore',
+            out_dir=os.path.join(args.output_directory, sequence_file_info.outname)
+        )
         for sequence_file_info in sequence_file_info_list
     ]
     runmetadata = RunMetadata(samples=samples)
     metadata = Metadata(runmetadata=runmetadata)
+
+    # Reports directory inside BestAssemblies/reports
     create_combinedmetadata_report(
         assemblies_dir=best_assemblies_dir,
-        reports_directory=os.path.join(os.path.abspath(best_assemblies_dir), 'reports'),
+        reports_directory=os.path.join(best_assemblies_dir, 'reports'),
         metadata=metadata
     )
     logging.info('Done!')
